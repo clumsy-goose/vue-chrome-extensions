7bd4d4adb4447dd3f289d8065971aec29419fe27
clumsy goose <12516122+clumsy-goose@user.noreply.gitee.com>
2024-02-26 02:10:39 +0800

    feat:封装svg图标

---
+++ src/sidePanel/components/icon/DownIcon.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="icon">
+        <svg width="15" height="15" viewBox="0 0 48 48" fill="#62969d"><path fill-rule="evenodd" clip-rule="evenodd" d="M24 31.728l16.264-16.264a1 1 0 011.414 0l1.414 1.415a1 1 0 010 1.414L25.414 35.97a2 2 0 01-2.828 0L4.908 18.293a1 1 0 010-1.414l1.414-1.415a1 1 0 011.415 0L24 31.728z" fill="#62969d"/></svg>
+    </div>
+</template>
+<style lang="less" scope> 
+</style>
