ae3fad071b74e05427629c617f33d2fc22b8ee72
clumsy goose <12516122+clumsy-goose@user.noreply.gitee.com>
2024-02-26 02:07:52 +0800

    feat:各翻译API的所需参数以及信息

---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+/**
+ * 百度翻译API
+ */
+const BaiDu = {
+    url:'http://api.fanyi.baidu.com/api/trans/vip/translate',
+    appid: '20240205001959649',
+    key: 'q3sapnWCaBDRWEliS7ir'
+}
+
+export{BaiDu}
