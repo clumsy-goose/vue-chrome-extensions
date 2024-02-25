5d323a78fb816b03ace15534d3cf7c0387a277c4
clumsy goose <12516122+clumsy-goose@user.noreply.gitee.com>
2024-02-26 02:14:22 +0800

    feat:封装提问（右侧）和回复（左侧）气泡框

---
+++ src/sidePanel/components/card/ApplyMessageCard.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+      <div class="chat-message-bot">
+        <div class="chat-message-bubble" >
+          <span>{{ sendType }}</span>
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+  </template>
+
+<script setup>
+const props = defineProps(['type']);
+const sendType = computed(()=>{
+    if(props.type === 'baidu'){
+        return "百度翻译";
+    }else if(props.type === 'youdao'){
+        return '有道翻译';
+    }
+})
+</script>
+
+  <style scoped lang="less">
+  
+  .chat-message-bubble {
+    font-size: 14px;
+    padding: 10px;
+    word-wrap: break-word;
+    white-space: normal;
+    word-wrap: break-word;
+    word-break: break-all;
+    text-align: left;
+    display: flex;
+    box-shadow: 0 8px 5px #919eab29;
+    flex-direction: column;
+    max-width: 95%;
+    margin-top: 10px;
+    margin-left: 10px;
+    margin-bottom: 10px;
+  }
+  
+  .chat-message-bot .chat-message-bubble {
+    background-color: rgb(246, 248, 250); // background-color: rgb(110, 129, 156);
+    border-radius: 10px 10px 10px 0px;
+    // box-shadow: 0 8px 5px #919eab29;
+    width: fit-content;
+    max-width: 90%;
+  }
+  
+  .chat-message-bubble--active:hover {
+    box-shadow: 2px 2px 2px #14161929; //  box-shadow: 0 5px 2px #14161929;
+    // cursor: pointer;
+  }
+  </style>
+  
---
+++ src/sidePanel/components/card/UserMessageCard.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <div class="chat-message-user chat-message">
+            <!-- <CardHead v-if="hasHead" role="user"></CardHead> -->
+            <div class="chat-message-bubble" >
+                <span>{{ sendType }}</span><br>
+                <slot></slot>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(['type']);
+const sendType = computed(()=>{
+    console.log(props.type)
+    if(props.type === 'user'){
+        return "您发送的文本为：";
+    }else{
+        return '您选中的文本为：';
+    }
+})
+</script>
+
+<style scoped lang="less">
+.chat-message-bubble {
+    padding: 10px;
+    word-wrap: break-word;
+    white-space: pre-line;
+    word-wrap: break-word;
+    word-break: break-all;
+    text-align: left;
+    margin-right: 10px;
+}
+
+.chat-message-bubble {
+    border-radius: 10px 10px 0px 10px;
+    background-color: rgb(110, 129, 156);
+    width: fit-content;
+    box-shadow: 0 8px 5px #919eab29;
+    color: white;
+    max-width: 100%;
+
+}
+
+.chat-message-bubble--active:hover {
+    box-shadow: 0 5px 2px #14161929;
+    cursor: pointer;
+}
+
+.chat-message-user {
+    align-items: flex-end;
+
+    &-head {
+        margin-right: 15px;
+    }
+}
+
+.chat-message {
+    display: flex;
+    flex-direction: column;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    max-width: 100%;
+}
+</style>
