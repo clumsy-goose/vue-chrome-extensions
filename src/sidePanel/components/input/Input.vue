74607e21b551341a468f3184a1592da5015ca1ec
clumsy goose <12516122+clumsy-goose@user.noreply.gitee.com>
2024-02-26 02:13:02 +0800

    feat:封装输入框

---
+++ src/sidePanel/components/input/Input.vue
@@ -0,0 +1,36 @@
+<template>
+        <textarea
+            v-foucs
+            autofocus
+            ref="textarea"
+            placeholder="Enter 换行"
+            spellcheck="false"
+            autocomplete="off"
+            class="chat-input-text"
+            v-model="inputMessage"
+            ></textarea>
+</template>
+
+<script setup>
+const inputMessage = defineModel()
+</script>
+
+<style lang="less" scoped>
+
+
+.chat-input-text {
+  resize: none;
+  box-sizing: border-box;
+  outline: none;
+  border-width: 0px;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  height: 100px;
+  width: 100%;
+  color:black;
+  background-color: var(--vt-c-white-mute);
+  border-radius: 15px;
+}
+
+</style>
